75ab6c06924ee9ce6c76f2b5dca4cc6ac9e03d47
Will <doggy.huang@gmail.com>
2021-12-14 16:10:50 +0800

    示範結構型指令 *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-cathay-211214';
   keyword = '123'
+
+  data: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any[]>('/api/articles.json').subscribe(data => {
+      this.data = data;
+    });
+  }
 
   clearKeyword() {
     this.keyword = '';
