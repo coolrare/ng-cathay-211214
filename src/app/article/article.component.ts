bc34d01761573576838c60e2f41119705e598e1d
Will <doggy.huang@gmail.com>
2021-12-16 09:40:20 +0800

    示範 Hood Methods 使用方式

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Article } from '../types/Article';
 
 @Component({
@@ -6,7 +6,7 @@ import { Article } from '../types/Article';
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit, OnChanges {
 
   @Input() item: Article = <Article>{};
 
@@ -15,6 +15,13 @@ export class ArticleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log('');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['item'].isFirstChange()) {
+      console.log(changes['item'].previousValue, changes['item'].currentValue);
+    }
   }
 
   doDelete() {
